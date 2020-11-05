81a71e60229032913b5348163c41d851827bc1e0
Dimuthu Fernando <dimuthuchalani996@gmail.com>
2020-11-05 22:29:21 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function MyApp(){
+  return(
+  <div>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </div>
+  )
+}
+
+
+ReactDOM.render(<MyApp/>,document.getElementById("root"))
